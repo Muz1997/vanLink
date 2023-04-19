a138de003cf26f216d142f35b91badd949abfb89
muzam <muzamali1997@gmail.com>
2023-04-19 15:29:33 +0100

    quotation, contact us

---
+++ public/assets/js/contactus.js
@@ -0,0 +1,32 @@
+const contactForm = document.querySelector("#contactForm");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let message = document.getElementById("message");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/contact");
+  xhr.setRequestHeader("content-type", "application/json");
+  console.log(xhr.responseText);
+  xhr.onload = function () {
+    if (xhr.responseText == "success") {
+      alert("Email Sent, we will get back with you as soon as posible.");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong. Please call us directly. Thank you.");
+    }
+  };
+  xhr.send(JSON.stringify(formData));
+});
