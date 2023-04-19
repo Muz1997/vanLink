560f049004515019425d85e99f02c3fe04eac908
muzam <muzamali1997@gmail.com>
2023-04-19 15:28:49 +0100

    Email function settled, quoation in progress

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+var xoauth2 = require("xoauth2");
+var SMTPTransport = require("nodemailer/lib/smtp-transport");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+const log = console.log;
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -23,6 +30,44 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
+app.post("/contact", (req, res) => {
+  log(req.body);
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    service: "outlook", // service name
+    secureConnection: false,
+    tls: {
+      ciphers: "SSLv3", // tls version
+    },
+    port: 587, // port
+    auth: {
+      user: "vanlinklogistics@outlook.com",
+      pass: "Hamidpiya12",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: "vanlinklogistics@outlook.com",
+    to: "vanlinklogistics@outlook.com",
+    subject:
+      "Subject : " + `Message from ${req.body.email}: ${req.body.subject}`,
+    text: `From ${req.body.email} \nSubject: ${req.body.subject} \nMessage : ${req.body.message}`,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+  });
+});
+
 app.get("/services", (req, res) => {
   res.render("services");
 });
@@ -31,6 +76,6 @@ app.get("/quote", (req, res) => {
   res.render("quote");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000");
+app.listen(PORT, () => {
+  log("Example app listening on port ", PORT);
 });
