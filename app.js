75fae1c3c934bd7ca49c630d4bd78b218a934822
muzam <muzamali1997@gmail.com>
2023-04-19 23:39:31 +0100

    quoatation implimented

--- app.js
+++ app.js
@@ -76,6 +76,42 @@ app.get("/quote", (req, res) => {
   res.render("quote");
 });
 
+app.post("/quote", (req, res) => {
+  log(req.body);
+  let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    service: "outlook", // service name
+    secureConnection: false,
+    tls: {
+      ciphers: "SSLv3", // tls version
+    },
+    port: 587, // port
+    auth: {
+      user: "vanlinklogistics@outlook.com",
+      pass: "Hamidpiya12",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: "vanlinklogistics@outlook.com",
+    to: "vanlinklogistics@outlook.com",
+    subject: "Subject: " + `Quotation Request from ${req.body.name}`,
+    text: `From ${req.body.name}\nEmail: ${req.body.email}    Telephone: ${req.body.Telephone}  \nSubject: Quotation \nDeparture City : ${req.body.departureCity}   Post Code : ${req.body.postCode}\nService Category : ${req.body.serviceCat}\nPackage Informtaion Weight: ${req.body.weigth} Height: ${req.body.height} Width: ${req.body.width} Length : ${req.body.length}\nVan Required : ${req.body.selectVan}`,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+  });
+});
+
 app.listen(PORT, () => {
   log("Example app listening on port ", PORT);
 });
--- public/assets/js/quotation.js
+++ public/assets/js/quotation.js
@@ -1,33 +1,55 @@
-const contactForm = document.querySelector("#contactForm");
+const quotationForm = document.querySelector("#quotationForm");
 let name = document.getElementById("name");
 let email = document.getElementById("email");
-let subject = document.getElementById("subject");
-let message = document.getElementById("message");
+let telephone = document.getElementById("Telephone");
+let serviceCat = document.getElementById("select1").value;
+let departureCity = document.getElementById("departureCity");
+let postCode = document.getElementById("postCode");
+let weigth = document.getElementById("weight");
+let height = document.getElementById("height");
+let width = document.getElementById("width");
+let length = document.getElementById("length");
+let selectVan = document.getElementById("select2").value;
 
-contactForm.addEventListener("submit", (e) => {
+quotationForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let formData = {
     name: name.value,
     email: email.value,
-    subject: subject.value,
-    message: message.value,
+    telephone: telephone.value,
+    departureCity: departureCity.value,
+    postCode: postCode.value,
+    serviceCat: serviceCat,
+    weigth: weigth.value,
+    height: height.value,
+    width: width.value,
+    length: length.value,
+    selectVan: selectVan,
   };
 
+  console.log(formData);
+
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/contact");
+  xhr.open("POST", "/quote");
   xhr.setRequestHeader("content-type", "application/json");
-  xhr.onload ==
-    function () {
-      console.log(xhr.responseText);
-      if (xhr.responseText == "success") {
-        alert("Email Sent, we will get back with you as soon as posible.");
-        name.value = "";
-        email.value = "";
-        subject.value = "";
-        message.value = "";
-      } else {
-        alert("Something went wrong. Please call us directly. Thank you.");
-      }
-    };
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email Sent, we will get back with you as soon as posible.");
+      name.value = "";
+      email.value = "";
+      telephone.value = "";
+      departureCity.value = "";
+      postCode.value = "";
+      serviceCat.value = "";
+      weigth.value = "";
+      height.value = "";
+      width.value = "";
+      length.value = "";
+      selectVan.value = "";
+    } else {
+      alert("Something went wrong. Please call us directly. Thank you.");
+    }
+  };
   xhr.send(JSON.stringify(formData));
 });
